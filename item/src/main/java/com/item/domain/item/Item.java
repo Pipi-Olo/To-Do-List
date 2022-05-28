740b6f43eeb987092fcef416809f635dac1095be
pipiolo <Copy.H13@gmail.com>
2022-05-29 02:21:44 +0900

    Comment 도메인 추가 (댓글)

--- item/src/main/java/com/item/domain/item/Item.java
+++ item/src/main/java/com/item/domain/item/Item.java
@@ -1,10 +1,12 @@
 package com.item.domain.item;
 
-import lombok.Getter;
-import lombok.Setter;
+import com.item.domain.comment.Comment;
+import lombok.Data;
 
-@Setter
-@Getter
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
 public class Item {
 
     private Long id;
@@ -12,11 +14,32 @@ public class Item {
     private Integer price;
     private Integer quantity;
 
+    private List<Comment> comments;
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+        this.comments = new ArrayList<>();
+    }
+
+    public Item(String itemName, Integer price, Integer quantity, List<Comment> comments) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+        this.comments = comments;
+    }
+
     public Item(Long id, String itemName, Integer price, Integer quantity) {
         this.id = id;
         this.itemName = itemName;
         this.price = price;
         this.quantity = quantity;
+        this.comments = new ArrayList<>();
+    }
+
+    public void addComment(Comment comment) {
+        this.comments.add(comment);
     }
 
     public void update(Item update) {
