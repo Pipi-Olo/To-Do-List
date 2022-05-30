fe1bdc1bcbb87382d8441cea637e71410062e6ec
pipiolo <Copy.H13@gmail.com>
2022-05-30 14:29:06 +0900

    JpaRepositoryV2 도입 및 MemRepository V1 삭제

---
+++ item/src/main/java/com/item/domain/user/UserRepositoryV2.java
@@ -0,0 +1,26 @@
+package com.item.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepositoryV2 extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    @Override
+    Optional<User> findById(Long aLong);
+
+    @Override
+    List<User> findAll();
+
+    @Override
+    <S extends User> S save(S entity);
+
+    @Override
+    void deleteById(Long aLong);
+
+    @Override
+    void deleteAll();
+}
