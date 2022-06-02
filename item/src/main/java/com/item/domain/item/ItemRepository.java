d4d4ec055e42151126cbf538eb805d97372bef67
pipiolo <Copy.H13@gmail.com>
2022-06-02 18:03:25 +0900

    소소한 수정

--- item/src/main/java/com/item/domain/item/ItemRepository.java
+++ item/src/main/java/com/item/domain/item/ItemRepository.java
@@ -2,26 +2,5 @@ package com.item.domain.item;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface ItemRepository extends JpaRepository<Item, Long> {
-
-    @Override
-    Optional<Item> findById(Long itemId);
-
-    @Override
-    List<Item> findAll();
-
-    @Override
-    <S extends Item> S save(S Item);
-
-    @Override
-    void delete(Item item);
-
-    @Override
-    void deleteById(Long itemId);
-
-    @Override
-    void deleteAll();
 }
--- item/src/main/java/com/item/domain/user/UserRepository.java
+++ item/src/main/java/com/item/domain/user/UserRepository.java
@@ -2,25 +2,9 @@ package com.item.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
-
-    @Override
-    Optional<User> findById(Long aLong);
-
-    @Override
-    List<User> findAll();
-
-    @Override
-    <S extends User> S save(S entity);
-
-    @Override
-    void deleteById(Long aLong);
-
-    @Override
-    void deleteAll();
 }
