6f5c32e2d937327b0c6f8e2bcad2bffcabad9cde
pipiolo <pipiolo1024@gmail.com>
2022-07-20 18:28:04 +0900

    Initial refactoring

---
+++ itemShop/src/main/java/com/pipiolo/itemshop/ItemShopApplication.java
@@ -0,0 +1,13 @@
+package com.pipiolo.itemshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemShopApplication.class, args);
+    }
+
+}
