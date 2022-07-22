6dbdcfde638f556b1041e2cb0fd3adcf282821ae
pipiolo <pipiolo1024@gmail.com>
2022-07-22 12:49:06 +0900

    회원가입 기능 추가

--- itemShop/src/main/java/com/pipiolo/itemshop/web/user/UserController.java
+++ itemShop/src/main/java/com/pipiolo/itemshop/web/user/UserController.java
@@ -1,10 +1,13 @@
 package com.pipiolo.itemshop.web.user;
 
-import com.pipiolo.itemshop.domain.delivery.Address;
-import com.pipiolo.itemshop.web.order.OrderSaveForm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
@@ -14,8 +17,21 @@ public class UserController {
 
     private final UserService userService;
 
-    @GetMapping
-    public void test() {
-        userService.save(new UserSaveForm("test", "test"));
+    @GetMapping("/add")
+    public String addUserForm(Model model) {
+        model.addAttribute("user", new UserSaveForm());
+        return "user/addUserForm";
+    }
+
+    @PostMapping("/add")
+    public String addUser(@Validated @ModelAttribute("user") UserSaveForm saveForm,
+                          BindingResult bindingResult
+    ) {
+        if (bindingResult.hasErrors()) {
+            return "user/addUserForm";
+        }
+
+        userService.save(saveForm);
+        return "redirect:/login";
     }
 }
--- itemShop/src/main/java/com/pipiolo/itemshop/web/user/UserSaveForm.java
+++ itemShop/src/main/java/com/pipiolo/itemshop/web/user/UserSaveForm.java
@@ -3,10 +3,11 @@ package com.pipiolo.itemshop.web.user;
 import com.pipiolo.itemshop.domain.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
-@Data @AllArgsConstructor
+@Data @AllArgsConstructor @NoArgsConstructor
 public class UserSaveForm {
 
     @NotBlank
