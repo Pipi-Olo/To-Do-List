226a688ea10031b6bd026d013a1a1a5569e2aa0d
pipiolo <pipiolo1024@gmail.com>
2022-07-22 12:20:15 +0900

    글로벌 런타임 예외 추가

--- itemShop/src/main/java/com/pipiolo/itemshop/domain/item/Item.java
+++ itemShop/src/main/java/com/pipiolo/itemshop/domain/item/Item.java
@@ -1,11 +1,14 @@
 package com.pipiolo.itemshop.domain.item;
 
 import com.pipiolo.itemshop.domain.BaseEntity;
+import com.pipiolo.itemshop.web.exception.GlobalException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+
+import static com.pipiolo.itemshop.web.exception.ErrorCode.*;
 
 @Getter @NoArgsConstructor @AllArgsConstructor
 @Entity
@@ -22,7 +25,7 @@ public class Item extends BaseEntity {
     public void removeStock(int quantity) {
         int rest = stockQuantity - quantity;
         if (rest < 0) {
-
+            throw new GlobalException(BAD_REQUEST, "재고 수량이 부족합니다.");
         }
         this.stockQuantity = rest;
     }
---
+++ itemShop/src/main/java/com/pipiolo/itemshop/web/exception/ErrorResult.java
@@ -0,0 +1,12 @@
+package com.pipiolo.itemshop.web.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorResult {
+
+    private String message;
+}
+
